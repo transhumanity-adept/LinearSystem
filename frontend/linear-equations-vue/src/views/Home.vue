b44c08a85a731f3f1b7565f398017a7ecf1b6617
- <d.b_official@mail.ru>
2022-06-19 15:16:21 +0300

    initial

---
+++ frontend/linear-equations-vue/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-icon>mdi-plus</v-icon>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: function() {
+    return {
+
+    }
+  },
+  mounted() {
+    this.axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then(response => console.log(response));
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
